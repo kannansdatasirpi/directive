108df339b73e430ca7f03916c07290e5c68a400e
kannan <kannans@datasirpi.com>
2022-11-28 09:39:08 +0530

    cus-dir

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div><CusDir/></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CusDir from './components/CusDir.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CusDir
+  },
+  data(){
+return{
+
+}
   }
 }
 </script>
---
+++ src/components/CusDir.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div>
+ <h2 v-focus="'red'">this is a sample text</h2>
+ <p v-focus="'green'">testing</p>
+</div>
+</template>
+ 
+<script>
+// import Vue from 'vue'
+// Vue.directive('colorful', {
+//   inserted(el,binding) {
+//     el.style.color = binding.value;
+
+//   }
+// })
+
+export default {
+  name: "CusDirs",
+  directives: {
+  focus: {
+   mounted: (el, binding) => {
+   // initiate focus for the element 
+   el.style.color = binding.value;
+  },
+ },
+}
+}
+</script>
+
+
+<style>
+
+</style>
