0eadac4257c0d9808898ea614c069d24f915fc98
kannan <kannans@datasirpi.com>
2022-11-29 20:16:23 +0530

    command added

--- src/components/CusDir.vue
+++ src/components/CusDir.vue
@@ -1,10 +1,22 @@
 <template>
  <h2 v-focus="'red'">this is a sample text</h2>
  <p v-test="'green'">testing</p>
+ <h2 v-colorful.orange>hi this is attached to a new custom directive</h2>
 </template>
  
 <script>
+// import Vue from 'vue'
+// Vue.directive('colorful',{
+//   inserted(el,binding) {
+//     if (binding.modifiers.red){
+//       el.style.color ='red'
+//     }else {
+//       el.style.color = binding.value
+//     }
+//   el.style.color = binding.value;
 
+//   }
+// })
 export default {
   name: "CusDir",
   directives: {
@@ -14,6 +26,17 @@ export default {
    el.style.color = binding.value;
   },
  },
+ colorful: {
+  mounted: (el,binding) => {
+    if (binding.modifiers.red){
+      el.style.color ='red'
+    }
+    else {
+      el.style.color = binding.value
+    }
+  // el.style.color = binding.value;
+  }
+ }
 }
 }
 </script>
